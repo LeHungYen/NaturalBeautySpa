9f7631e641fd515eacd37bb5df13ec831ad0725f
fromWork <quanhoang765e@gmail.com>
2024-04-05 16:43:41 +0700

    temp

---
+++ src/layouts/DefaultLayout/Footer/index.js
@@ -0,0 +1,195 @@
+import style from './index.module.scss'
+import { ImHome } from "react-icons/im";
+import { FaInstagram } from "react-icons/fa";
+import { FaWifi } from "react-icons/fa";
+import HomeItem from '../../../components/HomeItem';
+import { useState, useEffect } from 'react';
+import store from "../../../store/store";
+import Loading from "../../../components/Loading/loading";
+import { routes } from '../../../config/routes';
+import { useNavigate, Link } from 'react-router-dom';
+import { getDict } from '../../../services/dict';
+function Footer() {
+    const navigate = useNavigate();
+    const row1Item = [
+        {
+            img: "https://mareve.co.jp/wp-content/uploads/2020/10/pixta_69294004_M.jpg",
+            textBelow: getDict("defaultLayout_row1_item1"),
+            url: routes.about_access
+        },
+        {
+            img: "https://mareve.co.jp/wp-content/uploads/2020/10/pixta_23376677_M-e1603864881118.jpg",
+            textBelow: getDict("defaultLayout_row1_item2"),
+            url: routes.mainMenu
+        },
+        {
+            img: "https://mareve.co.jp/wp-content/uploads/2020/10/pixta_21787413_M.jpg",
+            textBelow: getDict("defaultLayout_row1_item3"),
+            url: routes.company
+        }
+    ]
+
+    // check window width
+    const [windowWidth, setWindowWidth] = useState(null);
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    //end
+
+
+    // Update font size based on window width
+    const [row1FS, setRow1FS] = useState(20)
+    const [textBelowH, setTextBelowH] = useState(60)
+    useEffect(() => {
+        if (windowWidth < 750) {
+            setRow1FS(16);
+        } else {
+            setRow1FS(20);
+        }
+
+        if (windowWidth < 550) {
+            setTextBelowH(50);
+        } else {
+            setTextBelowH(60)
+        }
+    }, [windowWidth]);
+    return (
+        <div className={style.container}>
+            <div className={style.row1}>
+                <div className={style.items}>
+                    {row1Item.map((item, index) => {
+                        return (
+                            <div key={index} className={style.item}>
+                                <a href={item.url}>
+                                    <HomeItem infor={item} textBelowH={textBelowH} textBelowFS={row1FS} textBelowC="#2bb8d1" />
+                                </a>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+
+            <div className={style.row2}>
+                <div className={style.displayFlex}>
+                    <div className={style.col1}>
+                        <a href={routes.home}>
+                            <div className={style.logo}>
+                                <img src={require('../../../assets/logoWhite.jpg')} alt="logo" />
+                                <p className={style.logoText}>Natural Beauty</p>
+                            </div>
+                        </a>
+
+                        <ul>
+                            <li><span>{getDict("defaultLayout_row2_col1_item1")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item2")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item3")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item4")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item5")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item6")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item7")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item8")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item9")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col1_item10")}</span></li>
+                        </ul>
+
+                    </div>
+
+                    <div className={style.col2}>
+                        <p className={style.title}>{getDict("defaultLayout_row2_col2_item1")}</p>
+
+                        <ul>
+                            <li><span>{getDict("defaultLayout_row2_col2_item2")}</span></li>
+                            <li><span>{getDict("defaultLayout_row2_col2_item3")}</span></li>
+                        </ul>
+
+                        <div className={style.button}>
+                            <button onClick={() => window.open("https://line.me/R/ti/p/%40737fasnn")}><a >{getDict("defaultLayout_row2_col2_item4")}</a></button>
+                        </div>
+                    </div>
+
+                    <div className={style.col3}>
+                        <p className={style.title}>
+                        {getDict("defaultLayout_row2_col3_item1")}
+                            <br />
+                            {getDict("defaultLayout_row2_col3_item2")}
+                        </p>
+
+                        <ul>
+                            <li><span>{getDict("defaultLayout_row2_col3_item3")}</span></li>
+                        </ul>
+
+                        <div className={style.button}>
+                            <button onClick={() => window.open("https://line.me/R/ti/p/%40737fasnn")}><a>{getDict("defaultLayout_row2_col3_item4")}</a></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.row3}>
+                <div className={style.displayFlex}>
+                    <div className={style.col1}>
+                        <div className={style.home}>
+                            <ImHome className={style.icon} onClick={() => navigate(routes.home)} />
+                        </div>
+
+                        <ul>
+                            <li><a href={routes.about}>{getDict("defaultLayout_row3_col1_item1")}</a></li>
+                            <li><a href={routes.mainMenu}>{getDict("defaultLayout_row3_col1_item2")}</a></li>
+                            <li><a>{getDict("defaultLayout_row3_col1_item3")}</a></li>
+                            <li><a href={routes.product}>{getDict("defaultLayout_row3_col1_item4")}</a></li>
+                            <li><a>{getDict("defaultLayout_row3_col1_item5")}</a></li>
+                            <li><a href={routes.company}>{getDict("defaultLayout_row3_col1_item6")}</a></li>
+                            <li><a href={routes.news}>{getDict("defaultLayout_row3_col1_item7")}</a></li>
+                            <li><a href={routes.privacyPolicy}>{getDict("defaultLayout_row3_col1_item8")}</a></li>
+                            <li><a href={routes.about_access}>{getDict("defaultLayout_row3_col1_item9")}</a></li>
+                        </ul>
+                    </div>
+
+                    <div className={style.col2}>
+                        <div className={style.title} >
+                            <a href={routes.mainMenu}><p>{getDict("defaultLayout_row3_col2_item1")}</p></a>
+                        </div>
+
+                        <ul>
+                            <li><a href={`${routes.menu}?key=0`}>{getDict("defaultLayout_row3_col2_item2")}</a></li>
+                            <li><a href={`${routes.menu}?key=1`}>{getDict("defaultLayout_row3_col2_item3")}</a></li>
+                            <li><a href={`${routes.menu}?key=2`}>{getDict("defaultLayout_row3_col2_item4")}</a></li>
+                            <li><a href={`${routes.menu}?key=3`}>{getDict("defaultLayout_row3_col2_item5")}</a></li>
+                            <li><a href={`${routes.menu}?key=4`}>{getDict("defaultLayout_row3_col2_item6")}</a></li>
+                            <li><a href={`${routes.menu}?key=5`}>{getDict("defaultLayout_row3_col2_item7")}</a></li>
+                            <li><a href={`${routes.menu}?key=6`}>{getDict("defaultLayout_row3_col2_item8")}</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div className={style.row4}>
+                <div className={style.displayFlex}>
+                    <div className={style.social}>
+                        <ul>
+                            <li><FaInstagram className={style.instagramIcon} /></li>
+                            <li><FaWifi className={style.wifiIcon} /></li>
+                        </ul>
+                    </div>
+
+                    <div className={style.copyright}>
+                        <p>{getDict("defaultLayout_row4_col2_item1")}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/layouts/DefaultLayout/header/index.js
@@ -0,0 +1,187 @@
+import style from './index.module.scss'
+import NavItem from "./NavItem/nav-item";
+import { getDict } from "../../../services/dict";
+import { useState, useRef } from "react";
+import NavigationBarDrawerMode from "./drawe/drawer";
+import { getAboutData } from "../../../pages/About";
+import AboutItem from "../../../components/AboutItem";
+import { useDispatch } from "react-redux";
+import { routes } from "../../../config/routes.js"
+import { useNavigate } from 'react-router-dom';
+import Login from '../../../components/Login/index.js';
+export default function Header(props) {
+    const navigate = useNavigate();
+    let { navigations } = props
+    const [showPopup, setShowPopup] = useState("none");
+    const items = getAboutData();
+    const dispatch = useDispatch();
+    const togglePopup = function (show, e) {
+        if (show) {
+            setShowPopup("block");
+        } else {
+            setShowPopup("none");
+        }
+    }
+    if (!navigations) {
+        navigations = [
+            {
+                name: getDict("nav-about-company"),
+                url: "/about",
+                items: [
+                    {
+                        caption: getDict("about-item-1-title"),
+                        url: "/about/message",
+                    },
+                    {
+                        caption: getDict("about-item-2-title"),
+                        url: "/about/feature",
+                    },
+                    {
+                        caption: getDict("about-item-3-title"),
+                        url: "/about/important",
+                    },
+                    {
+                        caption: getDict("about-item-4-title"),
+                        url: "/about/academy",
+                    },
+                    {
+                        caption: getDict("about-item-5-title"),
+                        url: "/about/product",
+                    },
+                    {
+                        caption: getDict("about-item-6-title"),
+                        url: "/about/access",
+                    },
+                ]
+            },
+            {
+                name: getDict("nav-hair-remover"),
+                url: routes.mainMenu,
+                items: [
+                    {
+                        caption: getDict("nav_drawer_sub_2_1"),
+                        url: `${routes.menu}?key=0`,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-2-2"),
+                        url: `${routes.menu}?key=1`,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-2-3"),
+                        url: `${routes.menu}?key=2`,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-2-4"),
+                        url: `${routes.menu}?key=3`,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-2-5"),
+                        url: `${routes.menu}?key=4`,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-2-6"),
+                        url: `${routes.menu}?key=5`,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-2-7"),
+                        url: `${routes.menu}?key=6`,
+                    },
+                ]
+            },
+            {
+                name: getDict("nav-product"),
+                url: "/product"
+            },
+            // {
+            //     name: getDict("nav-customer-feedback"),
+            //     url: "/"
+            // },
+            {
+                name: getDict("nav-company-profile"),
+                url: "/company",
+                items: [
+                    {
+                        caption: getDict("nav-drawer-sub-3-1"),
+                        url: routes.news,
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-3-2"),
+                        url: "/contact",
+                    },
+                    {
+                        caption: getDict("nav-drawer-sub-3-3"),
+                        url: routes.privacyPolicy,
+                    },
+                ]
+            },
+            {
+                name: getDict("nav-reservation"),
+                url: "/reservation"
+            }
+        ]
+    }
+
+    const loginRef = useRef(null);
+    const exitLogin = () => {
+        loginRef.current.style.display = "none";
+    }
+    const openLogin = () => {
+        loginRef.current.style.display = "block";
+    }
+    return (
+        <div className={style.header} >
+            <div className={style.top}>
+                <div className={style.items}>
+                    <a href={routes.home}>
+                        <div className={style.logo}>
+                            <img src={require('../../../assets/logoWhite.jpg')} alt="logo" />
+                            <p className={style.logoText}>Natural Beauty</p>
+                        </div>
+                    </a>
+                    <div className={style.messenger}>
+                        <span>{getDict("messenger-reservation")}</span>
+                    </div>
+                    <NavigationBarDrawerMode navigations={navigations} />
+                </div>
+            </div>
+            <div className={style.navigation}>
+                <div className={style.container}>
+                    {navigations.map((nav, idx) => {
+                        if (idx === 0) return <NavItem {...nav} callBack={togglePopup} index={idx} />
+                        return <NavItem {...nav} index={idx} />
+                    }
+                    )}
+                </div>
+            </div>
+            <div className={style.intro} id="about" style={{ display: showPopup }}
+                onMouseEnter={(e) => togglePopup(true, e)}
+                onMouseLeave={(e) => togglePopup(false, e)}
+            >
+                <div className={style.container}>
+                    <div className={style.about}>
+                        <div className={style.title}>
+                            <h3>About Mareve</h3>
+                            <p>{getDict("about-popup-sub-title")}</p>
+                        </div>
+                        <div className={style.detail}>
+                            {getDict("about-popup-description")}
+                        </div>
+                        <button>{getDict("about-popup-btn")}</button>
+                    </div>
+                    <div className="about-item-popup">
+                        {items.map((item, idx) =>
+                            <div key={idx} onClick={() => { window.location.href = item.url }}>
+                                <AboutItem {...item} popup={true} zoomEffect={true} />
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+
+            <div className={style.login}>
+                <button onClick={openLogin}>Login</button>
+            </div>
+            <Login exitLogin={exitLogin} loginRef={loginRef} />
+        </div>
+    )
+}
